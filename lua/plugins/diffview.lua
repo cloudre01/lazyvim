77168c8690e46ff875a626785417811a8a3d97a4
cloudre01 <alextan01@hotmail.com>
2025-06-20 07:21:38 +0800

    add keymaps to diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -8,5 +8,38 @@ return {
         },
       },
     },
+    keys = {
+      {
+        "<leader>gdb",
+        function()
+          require("telescope.builtin").git_branches({
+            prompt_title = "Select branch to diff against",
+            attach_mappings = function(_, map)
+              map("i", "<CR>", function(prompt_bufnr)
+                local actions = require("telescope.actions")
+                local state = require("telescope.actions.state")
+                local selection = state.get_selected_entry()
+                actions.close(prompt_bufnr)
+                if selection and selection.value then
+                  vim.cmd("DiffviewOpen " .. selection.value)
+                end
+              end)
+              return true
+            end,
+          })
+        end,
+        desc = "Diffview Branch",
+      },
+      {
+        "<leader>gdc",
+        "<cmd>DiffviewClose<cr>",
+        desc = "Close Diffview",
+      },
+      {
+        "<leader>gdf",
+        "<cmd>DiffviewFileHistory %<cr>",
+        desc = "File History",
+      },
+    },
   },
 }
